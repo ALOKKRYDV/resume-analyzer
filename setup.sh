533a774e5d1e21969afe9da213a2418bb3c9e19e
Aditya40104 <adityadav294@gmail.com>
2026-01-24 12:41:36 +0530

    Fix Render deployment - Remove spaCy from setup.sh, update numpy to 1.26.4

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@
 # Download NLTK data
 python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('wordnet'); nltk.download('averaged_perceptron_tagger')"
 
-# Download spaCy model
-python -m spacy download en_core_web_sm
-
 # Initialize database
 python -c "from app import app, db; app.app_context().push(); db.create_all()"
 
