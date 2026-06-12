e8613e32daea9794067c93e71692eac3a6e6b7c1
alok kumar <alok00284@gmail.com>
2026-06-12 12:50:02 +0530

    update

--- app.py
+++ app.py
@@ -250,6 +250,8 @@ def init_db():
         db.create_all()
         print("Database initialized!")
 
+# This runs whether gunicorn or python starts the app
+init_db()
+
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
